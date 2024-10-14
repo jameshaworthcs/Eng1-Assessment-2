0fb333faa43aa6b7487982d5ab5531004674d550
c1theo <theojcoleman@icloud.com>
2024-10-14 14:32:55 +0100

    Add files via upload

---
+++ BoxInfo.java
@@ -0,0 +1,2 @@
+package com.UniSim.game;public class BoxInfo {
+}
---
+++ Constants.java
@@ -0,0 +1,7 @@
+package com.UniSim.game;
+
+public final class Constants
+{
+
+    public static final float PPM = 32; // pixels per meter
+}
