4cc307225c8c2241debebd485d7940b7b0771a11
MatiasDuplock <Matias.duplock@gmail.com>
2024-10-30 22:00:24 +0000

    Update This
    Getting rid of some imports plus testing git

--- core/src/main/java/com/UniSim/game/UniSim.java
+++ core/src/main/java/com/UniSim/game/UniSim.java
@@ -2,30 +2,8 @@ package com.UniSim.game;
 
 
 import com.UniSim.game.Screens.gameScreen;
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
-import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-
-import java.util.ArrayList;
-
-import static com.UniSim.game.Constants.PPM;
 
 public class UniSim extends Game {
     public SpriteBatch batch;
@@ -33,7 +11,7 @@ public class UniSim extends Game {
     @Override
     public void create() {
         batch = new SpriteBatch();
-        setScreen(new gameScreen(this));
+        setScreen(new gameScreen(this)); 
     }
 
     @Override
