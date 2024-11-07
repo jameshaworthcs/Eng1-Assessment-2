1b9bf2a60e1cb9162c8f24ead3650b513900f605
MatiasDuplock <Matias.duplock@gmail.com>
2024-11-07 10:49:14 +0000

    changes aspect

--- core/src/main/java/com/UniSim/game/Constants.java
+++ core/src/main/java/com/UniSim/game/Constants.java
@@ -17,12 +17,12 @@ public final class Constants
     public static final float GRID_SIZE = 8;
 
     // Screen size and camera border calculations
-    public static final float SCREEN_SIZE_X = (Gdx.graphics.getWidth() - 100) / SCALE / PPM;
+    public static final float SCREEN_SIZE_X = (Gdx.graphics.getWidth() - 192) / SCALE / PPM;
     public static final float BORDER_SIZE_X = SCREEN_SIZE_X * 0.2f; // 20% border is 200 pixels
     public static final float CENTER_MIN_X = BORDER_SIZE_X;         // 200 pixels from edge
     public static final float CENTER_MAX_X = SCREEN_SIZE_X - BORDER_SIZE_X; // 800 pixels from edge
 
-    public static final float SCREEN_SIZE_Y = (Gdx.graphics.getHeight() - 100) / SCALE / PPM;
+    public static final float SCREEN_SIZE_Y = (Gdx.graphics.getHeight() - 108) / SCALE / PPM;
     public static final float BORDER_SIZE_Y = SCREEN_SIZE_Y * 0.3f; // 20% border is 200 pixels
     public static final float CENTER_MIN_Y = BORDER_SIZE_Y;         // 200 pixels from edge
     public static final float CENTER_MAX_Y = SCREEN_SIZE_Y - BORDER_SIZE_Y; // 800 pixels from edge
