21e709a1faf8635991599d1fff72823a09a9dd70
BUBB134 <benfaulkner2005@icloud.com>
2024-12-18 23:53:42 +0000

    Created event class
    Event class includes whether positive or negative, a description, and values to update player stats. Also includes a sound that is played that is specific to each event.

---
+++ core/src/main/java/com/UniSim/game/Events/Event.java
@@ -0,0 +1,68 @@
+package com.UniSim.game.Events;
+
+import com.UniSim.game.Stats.PlayerStats;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
+//TODO: add event sound into this class so each event can have its own sound
+
+/**
+ * The Event class represents an event that can occur in the game.
+ * Events can affect the player's stats, trigger other events, or change the
+ * game state.
+ */
+
+public class Event {
+    public enum EventType {
+        POSITIVE, NEGATIVE, NEUTRAL
+    }
+
+    private EventType type;
+    private String description;
+    private int scoreEffect;
+    private int moneyEffect;
+    private Sound eventSound;
+
+    /**
+     * Constructor for the Event class.
+     *
+     * @param type          The type of the event (positive, negative, neutral).
+     * @param description   The description of the event.
+     * @param scoreEffect   The effect on the player's score.
+     * @param moneyEffect   The effect on the player's money.
+     * @param soundFilePath The file path to the sound associated with the event.
+     */
+
+    public Event(EventType type, String description, int scoreEffect, int moneyEffect, String soundFilePath) {
+        this.type = type;
+        this.description = description;
+        this.scoreEffect = scoreEffect;
+        this.moneyEffect = moneyEffect;
+        this.eventSound = Gdx.audio.newSound(Gdx.files.internal(soundFilePath));
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getScoreEffect() {
+        return scoreEffect;
+    }
+
+    public float getMoneyEffect() {
+        return moneyEffect;
+    }
+
+    public void applyEffects(PlayerStats playerStats) {
+        playerStats.increaseSatisfaction(scoreEffect);
+        playerStats.increaseCurrency(moneyEffect);
+    }
+
+    public void playSound() {
+        eventSound.play();
+    }
+}
